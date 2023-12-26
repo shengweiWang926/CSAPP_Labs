6ed1f63ffabbfc7d1a71cc869a87e6c4a25ad03e
Shengwei Wang <shengwsw@umich.edu>
2023-12-25 23:02:18 -0500

    Some work on Cache Lab

--- Cache_Lab/cachelab-handout/csim.c
+++ Cache_Lab/cachelab-handout/csim.c
@@ -1,7 +1,267 @@
 #include "cachelab.h"
+#include <stdio.h>
+#include <getopt.h>
+#include <stdlib.h>
+#include <unistd.h> 
+#include <string.h>
+#include <errno.h>
+#include <stdint.h>
 
-int main()
-{
-    printSummary(0, 0, 0);
+#define INSTLEN 64
+
+// Performance Parameters
+int hit_count = 0;
+int miss_count = 0;
+int eviction_count = 0;
+
+typedef unsigned long ulong;
+
+
+typedef struct {
+    int valid;
+    int dirty;
+    ulong tag;
+    int lru_counter;
+} CacheLine;
+
+typedef CacheLine* CacheSet;
+typedef CacheSet* Cache;
+
+// typedef struct {
+//     cache_block block[SET_NUM];
+// } cache_set;
+
+typedef struct {
+    int s;
+    int E;
+    int b;
+    int hit_count;
+    int miss_count;
+    int eviction_count;
+} cache_param;
+
+// initialize cache size according to s, E, b
+Cache initCache (int setIndexBits, int assoc) {
+    Cache cache;
+    ulong setNum = 1 << setIndexBits;
+    
+    cache = calloc(setNum, sizeof(CacheSet));
+
+    for (int i = 0; i < setNum; i++){
+        cache[i] = calloc(assoc, sizeof(CacheLine));
+    }
+
+    for (int i = 0; i < setNum; i++){
+        for (int j = 0; j < assoc; j++){
+            cache[i][j].valid = 0;
+            cache[i][j].lru_counter = 0;
+        }
+    }
+    
+    return cache;
+}
+
+int getLruNum (ulong index, int assoc, Cache cache) {
+    int lru_num;
+    for (int i=0; i<assoc; i++) {
+        if (cache[index][i].lru_counter == 0) {
+            lru_num = i;
+        }
+    }
+
+    return lru_num; 
+}
+
+void cacheInsert (ulong tag, ulong index, int assoc, int block_num, int old_lru_count, Cache cache) {
+    cache[index][block_num].valid = 1;
+    cache[index][block_num].tag = tag;
+
+    if (old_lru_count != (assoc-1)) {
+        for (int i = 0; i < assoc; i++) {
+            if (i != block_num && cache[index][i].lru_counter > old_lru_count) {
+                cache[index][i].lru_counter--;
+            }
+        }
+        cache[index][block_num].lru_counter = assoc-1;
+    }
+}
+
+
+
+void checkCache (ulong tag, ulong index, int assoc, Cache cache, int verboseFlag){
+    
+    int cache_hit = 0;
+    int set_full = 1;
+    int hit_block_num = 0;
+    int hit_block_lru_old = 0;
+
+    int lru_num;
+
+    for (int i = 0; i < assoc; i++) {
+        if (cache[index][i].valid == 0) {
+            set_full = 0;
+        }
+
+        if (cache[index][i].tag == tag && cache[index][i].valid == 1){
+            cache_hit = 1;
+            hit_block_num = i;
+            hit_block_lru_old = cache[index][i].lru_counter;
+            break;
+        }
+    }
+
+    if (cache_hit == 1) { // HIT
+        hit_count++;
+        if (verboseFlag == 1){
+            printf("hit\n");
+        }
+        cacheInsert(tag, index, assoc, hit_block_num, hit_block_lru_old, cache);
+    } else if (cache_hit == 0 && set_full == 1){ // MISS, Cache Full, need EVICT
+        miss_count++;
+        eviction_count++;
+        if (verboseFlag == 1){
+            printf("miss\t");
+            printf("evict\n");
+        }
+        lru_num = getLruNum(index, assoc, cache);
+        cacheInsert(tag, index, assoc, lru_num, hit_block_lru_old, cache);
+    } else {  // MISS
+        miss_count++;
+        if (verboseFlag == 1){
+            printf("miss\n");
+        }
+        lru_num = getLruNum(index, assoc, cache);
+        cacheInsert(tag, index, assoc, lru_num, hit_block_lru_old, cache);
+    }
+    /*
+    if (cache_hit == 0) {   // miss
+        increase miss count
+        if (set is full)
+            evict the LRU one, 
+            change LRU
+            increase evict count
+        else 
+            store the tag in empty ones
+            set valid bit to 1
+            change LRU
+    }
+   
+    */
+}
+
+void usage () {
+    printf("Usage: ./csim [-hv] -s <num> -E <num> -b <num> -t <file>\n");
+    printf("Options:\n");
+    printf("  -h       Print this help message.\n");
+    printf("  -v       Optional verbose flag.\n");
+    printf("  -s <num> Number of set index bits.\n");
+    printf("  -E <num> Number of lines per set.\n");
+    printf("  -b <num> Number of block offset bits.\n");
+    printf("  -t <num> Trace file.\n");
+}
+
+
+
+int main(int argc, char *argv[]) {
+    int option;
+
+    // cache_param Param;
+
+    int setIndexBits = 0; // setIndexBits
+    int assoc = 0; // associativity
+    int blockBits = 0;
+    char *tracefile = NULL;
+    int verboseFlag = 0;
+
+    // Process command-line options using getopt
+    while ((option = getopt(argc, argv, "hvs:E:b:t:")) != -1) {
+        switch (option) {
+            case 'h':
+                usage();
+                break;
+            case 'v':
+                verboseFlag = 1;
+                break;
+            case 's':
+                setIndexBits = atoi(optarg);
+                break;
+            case 'E':
+                assoc = atoi(optarg); // optarg contains the value for option 'b'
+                break;
+            case 'b':
+                blockBits = atoi(optarg);
+                break;
+            case 't':
+                tracefile = optarg;
+                break;
+            case '?':
+                // if (optopt == 'b') {
+                //     fprintf(stderr, "Option -%c requires an argument.\n", optopt);
+                // } else {
+                    fprintf(stderr, "Unknown option -%c\n", optopt);
+                // }
+                // break;
+                return 1;
+            default:
+                abort();
+        }
+    }
+
+    // Process non-option arguments after the options
+    for (int i = optind; i < argc; i++) {
+        printf("Non-option argument: %s\n", argv[i]);
+    }
+    
+
+    Cache cache = initCache(setIndexBits,assoc);
+
+    FILE* pFile;
+    pFile = fopen(tracefile,"r");
+    if (pFile == NULL) {
+        printf("no such file");
+        return 1;
+    }
+
+    char identifier;
+    ulong address;
+    int size;
+
+    ulong tag;
+    ulong index;
+
+    while (fscanf(pFile," %c %lx,%d", &identifier, &address, &size) > 0) {
+        int tagBits = INSTLEN-blockBits-setIndexBits;
+        tag = address >> (blockBits + setIndexBits);
+        index = (address << tagBits) >> (tagBits + blockBits);
+        // printf("identifier:%c, address:%lx, tag:%lx, index:%lx\n", identifier, address, tag, index);
+        
+        if (verboseFlag == 1) {
+            printf("%c %lx,%d ", identifier, address, size);
+            printf("(Decimal) [index: %ld, tag: %ld] ------ ", index, tag);
+        }
+
+
+        switch (identifier)
+        {
+        case 'I':
+            break;
+        case 'M':
+            checkCache(tag, index, assoc, cache, verboseFlag);
+            checkCache(tag, index, assoc, cache, verboseFlag);
+            break;
+        case 'S':
+            checkCache(tag, index, assoc, cache, verboseFlag);
+            break;
+        case 'L':
+            checkCache(tag, index, assoc, cache, verboseFlag);
+            break;
+        default:
+            break;
+        }
+
+        // printf("hit:%d, miss:%d, eviction:%d\n\n",hit_count, miss_count, eviction_count);
+    }
+
+    printSummary(hit_count, miss_count, eviction_count);
     return 0;
 }
--- Cache_Lab/cachelab-handout/trans.c
+++ Cache_Lab/cachelab-handout/trans.c
@@ -22,6 +22,115 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
+    int row, col, row_tile; // , col_tile;
+    int t0,t1,t2,t3;
+    int t4,t5,t6,t7;
+    // int t8,t9,t10,t11,t12,t13,t14,t15;
+    // t16,t17,t18,t19;
+    // int t20,t21,t22,t23,t24,t25,t26,t27,t28,t29;
+    // int t30,t31; 
+    for (row = 0; row < N; row = row + 8) {
+        for (col = 0; col < M; col = col + 16) {
+            for (row_tile = row; row_tile < row+8; row_tile += 1) {
+                t0 = A[row_tile][col+0];
+                t1 = A[row_tile][col+1];
+                t2 = A[row_tile][col+2];
+                t3 = A[row_tile][col+3];
+                t4 = A[row_tile][col+4];
+                t5 = A[row_tile][col+5];
+                t6 = A[row_tile][col+6];
+                t7 = A[row_tile][col+7];
+                // t8 = A[row_tile][col+8];
+                // t9 = A[row_tile][col+9];
+                // t10 = A[row_tile][col+10];
+                // t11 = A[row_tile][col+11];
+                // t12 = A[row_tile][col+12];
+                // t13 = A[row_tile][col+13];
+                // t14 = A[row_tile][col+14];
+                // t15 = A[row_tile][col+15];
+                // t16 = A[row_tile][col+16];
+                // t17 = A[row_tile][col+17];
+                // t18 = A[row_tile][col+18];
+                // t19 = A[row_tile][col+19];
+                // t20 = A[row_tile][col+20];
+                // t21 = A[row_tile][col+21];
+                // t22 = A[row_tile][col+22];
+                // t23 = A[row_tile][col+23];
+                // t24 = A[row_tile][col+24];
+                // t25 = A[row_tile][col+25];
+                // t26 = A[row_tile][col+26];
+                // t27 = A[row_tile][col+27];
+                // t28 = A[row_tile][col+28];
+                // t29 = A[row_tile][col+29];
+                // t30 = A[row_tile][col+30];
+                // t31 = A[row_tile][col+31];
+
+                B[col+0][row_tile] = t0;
+                B[col+1][row_tile] = t1;
+                B[col+2][row_tile] = t2;
+                B[col+3][row_tile] = t3;
+                B[col+4][row_tile] = t4;
+                B[col+5][row_tile] = t5;
+                B[col+6][row_tile] = t6;
+                B[col+7][row_tile] = t7;
+                // B[col+8][row_tile] = t8;
+                // B[col+9][row_tile] = t9;
+                // B[col+10][row_tile] = t10;
+                // B[col+11][row_tile] = t11;
+                // B[col+12][row_tile] = t12;
+                // B[col+13][row_tile] = t13;
+                // B[col+14][row_tile] = t14;
+                // B[col+15][row_tile] = t15;
+                // B[col+16][row_tile] = t16;
+                // B[col+17][row_tile] = t17;
+                // B[col+18][row_tile] = t18;
+                // B[col+19][row_tile] = t19;
+                // B[col+20][row_tile] = t20;
+                // B[col+21][row_tile] = t21;
+                // B[col+22][row_tile] = t22;
+                // B[col+23][row_tile] = t23;
+                // B[col+24][row_tile] = t24;
+                // B[col+25][row_tile] = t25;
+                // B[col+26][row_tile] = t26;
+                // B[col+27][row_tile] = t27;
+                // B[col+28][row_tile] = t28;
+                // B[col+29][row_tile] = t29;
+                // B[col+30][row_tile] = t30;
+                // B[col+31][row_tile] = t31;
+
+                // for (col_tile = col; col_tile < col+32; col_tile += 1) {
+                //     tmp[col_tile-col] = A[row_tile][col_tile];
+                //     B[col_tile][row_tile] = tmp[col_tile-col];
+                // }
+            }
+        }
+    }
+
+    // for (int i = 0; i < N; i += 8) {
+    //     for (int j = 0; j < M; j += 8) {
+    //         for (int k = i; k < i + 8; k++) {
+    //             int t0 = A[k][j];
+    //             int t1 = A[k][j + 1];
+    //             int t2 = A[k][j + 2];
+    //             int t3 = A[k][j + 3];
+    //             int t4 = A[k][j + 4];
+    //             int t5 = A[k][j + 5];
+    //             int t6 = A[k][j + 6];
+    //             int t7 = A[k][j + 7];
+
+    //             B[j][k] = t0;
+    //             B[j + 1][k] = t1;
+    //             B[j + 2][k] = t2;
+    //             B[j + 3][k] = t3;
+    //             B[j + 4][k] = t4;
+    //             B[j + 5][k] = t5;
+    //             B[j + 6][k] = t6;
+    //             B[j + 7][k] = t7;
+    //         }
+    //     }
+    // }
+
+
 }
 
 /* 
